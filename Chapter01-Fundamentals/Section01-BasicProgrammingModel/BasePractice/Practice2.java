2172a6934b80bc196be6ec672ead619ce14c55a9
zhaokai-home <1005324930@qq.com>
2017-07-15 14:25:52 +0800

    Add practice 1.1.2 code.

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/BasePractice/Practice2.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdOut;
+/**
+ * Practice2
+ */
+public class Practice2 {
+
+    public static void main(String[] args) {
+        StdOut.printf("%f\n%f\n%b\n%s", (1 + 2.236)/2,1 + 2 + 3 + 4.0,4.1 >= 4, 1 + 2 + "3");
+    }
+}
