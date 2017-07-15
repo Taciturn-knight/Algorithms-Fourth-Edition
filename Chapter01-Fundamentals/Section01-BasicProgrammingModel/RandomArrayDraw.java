404ad496cb68ec8a52d5906a3f0b2797594c0164
zhaokai-home <1005324930@qq.com>
2017-07-15 10:35:30 +0800

    Add 1.1.9 draw code

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/RandomArrayDraw.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomArrayDraw{
+    public static void main(String[] args) {
+        int n = 50;
+        double[] a = new double[n];
+        for(int i=0; i<n; i++)
+        {
+            a[i] = StdRandom.random();
+        }
+        for(int i=0; i<n; i++)
+        {
+            double x = 1.0*i/n;
+            double y = a[i]/2.0;
+            double halfWidth = 0.5/n;
+            double halfHeight = a[i]/2.0;
+            StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+        }
+    }
+}
