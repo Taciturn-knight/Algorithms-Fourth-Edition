571b2baca194ec2aeafc2f535e2e866d07873cf7
zhaokai-home <1005324930@qq.com>
2017-07-15 15:37:03 +0800

    Add practice 1.1.24 code

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/BasePractice/Euclid.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.*;
+
+/**
+ * Euclid
+ */
+public class Euclid {
+	public static int euclid(int divisor, int dividend) {
+		StdOut.printf("divisor: %d\tdividend: %d\n", divisor,dividend);
+		int remainder = divisor%dividend;
+		if(remainder == 0){
+			return dividend;
+		}
+		return euclid(dividend, remainder);
+	}
+	public static void main(String[] args) {
+		int divisor = Integer.parseInt(args[0]);
+		int dividend = Integer.parseInt(args[1]);
+		StdOut.println(Euclid(divisor, dividend));
+    }
+}
