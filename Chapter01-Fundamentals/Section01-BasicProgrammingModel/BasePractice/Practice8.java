6e01f9d2ccd8c023e55b06320e933ac30b95a459
zhaokai-home <1005324930@qq.com>
2017-07-15 14:49:19 +0800

    Add practice 1.1.8 code.

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/BasePractice/Practice8.java
@@ -0,0 +1,10 @@
+/**
+ * Practice8
+ */
+public class Practice8 {
+    public static void main(String[] args) {
+		System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char)('a' + 4));
+    }
+}
