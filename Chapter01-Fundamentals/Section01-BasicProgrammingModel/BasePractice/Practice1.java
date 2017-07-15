2b20f505e457fcf9c6ef6d66ce11e32234ec041a
zhaokai-home <1005324930@qq.com>
2017-07-15 14:25:25 +0800

    Add practice 1.1.1.

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/BasePractice/Practice1.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.StdOut;
+/**
+ * Practice1
+ */
+public class Practice1 {
+    public static void main(String[] args) {
+		int one = (0 + 15)/2;
+        double two = 2.0e-6 * 100000000.1;
+        boolean three = true && false || true && true;
+        StdOut.printf("%d\n%.8f\n%b", one,two,three);	
+    }
+
+}
