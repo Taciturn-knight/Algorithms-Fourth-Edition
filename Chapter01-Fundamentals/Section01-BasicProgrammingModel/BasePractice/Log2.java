80da83800a05e7164fb91357812f311ac4315d7d
zhaokai-home <1005324930@qq.com>
2017-07-15 15:07:43 +0800

    Add practice 1.1.20 code

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/BasePractice/Log2.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.algs4.*;
+
+/**
+ * Log2
+ */
+public class Log2 {
+	public static double log2(long num) {
+		if(num == 1) return 0;
+		else return Math.log(num) + log2(num-1);
+	}
+
+    public static void main(String[] args) {
+		long num = StdIn.readLong();
+		StdOut.println(log2(num));
+    }
+}
