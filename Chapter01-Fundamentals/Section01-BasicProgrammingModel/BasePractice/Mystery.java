74ea45c8dd494cabfeaee718632ffb24c390e0ca
zhaokai-home <1005324930@qq.com>
2017-07-15 15:04:48 +0800

    Add practice 1.1.18 code

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/BasePractice/Mystery.java
@@ -0,0 +1,24 @@
+import edu.princeton.cs.algs4.*;
+
+/**
+ * Mystery
+ */
+public class Mystery {
+	public static int mystery(int a ,int b) {
+		if(b == 0) return 0;
+		if(b % 2 == 0) return mystery(a+a, b/2);
+		return mystery(a+a, b/2) + a;
+	}
+
+	public static int mystery1(int a, int b) {
+		if(b == 0) return 0;
+		if(b % 2 == 0) return mystery1(a*a, b/2);
+		return mystery1(a*a, b/2) * a;
+	}
+    public static void main(String[] args) {
+		System.out.println(mystery(2, 25));
+		System.out.println(mystery(3, 11));
+		System.out.println(mystery1(2, 25));
+		System.out.println(mystery1(3, 11));
+    }
+}
