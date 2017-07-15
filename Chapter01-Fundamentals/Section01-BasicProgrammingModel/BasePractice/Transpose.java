ded443ce040ef28e60ba5b6013df35109f6fb87e
zhaokai-home <1005324930@qq.com>
2017-07-15 14:58:23 +0800

    Add practice 1.1.13 code

---
+++ Chapter01-Fundamentals/Section01-BasicProgrammingModel/BasePractice/Transpose.java
@@ -0,0 +1,23 @@
+/**
+ * Transpose
+ */
+public class Transpose {
+    public static void main(String[] args) {
+		int[][] num = {{1,2,3},{4,5,6},{7,8,9}};
+		int temp = 0;
+		for(int i=0; i<3; i++){
+			for(int j=i; j<3; j++){
+				temp = num[i][j];
+				num[i][j] = num[j][i];
+				num[j][i] = temp;
+			}
+		}
+
+		for(int i = 0; i<3; i++){
+			System.out.println();
+			for(int j = 0; j<3; j++){
+				StdOut.printf("%d\t",num[i][j]);
+			}
+		}
+    }
+}
